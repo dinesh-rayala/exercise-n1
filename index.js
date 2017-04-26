a4ed18b5c13108f9d8524029489479f052c31c32
dinesh c. rayala <dinesh.rayala@gmail.com>
2017-04-25 19:32:37 -0700

    Update

---
+++ index.js
@@ -0,0 +1,29 @@
+const pg = require('pg');
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.redirect('/db');
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM test_table', function (err, result) {
+      done();
+      if (err) {
+        console.error("An error occurred on the server. Error: " + err);
+      } else {
+        response.render('pages/db', { rows : result.rows });
+      }
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${PORT} ...`);
+});
